d9f0df785540e51b01d7375ebb311efd8bd54404
rania-1999 <rania.larafa@esprit.tn>
2022-03-08 23:41:42 +0100

    commit1

---
+++ PiDev-business-and-travel/src/main/java/tn/esprit/spring/entities/ParticipationEvenement.java
@@ -0,0 +1,43 @@
+package tn.esprit.spring.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@RequiredArgsConstructor
+public class ParticipationEvenement implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int idParticipEvenement;
+	
+	@NonNull private java.util.Date dateParticipation;
+	@ToString.Exclude
+	@ManyToOne
+	private Entreprise entreprises;
+	 @ToString.Exclude
+		@ManyToOne
+		private Evenement evenements;
+
+}
